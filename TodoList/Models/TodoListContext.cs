d3fcb6690e4c38c66dee0fc8fde20906deb5ea17
John Mondragon <jmondragon@hugeinc.com>
2016-03-04 10:22:05 -0500

    adding users

--- TodoList/Models/TodoListContext.cs
+++ TodoList/Models/TodoListContext.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
 
 namespace TodoList.Models
 {
     public class TodoListContext : DbContext
     {
         public DbSet<Task> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
 
     }
 }
---
+++ TodoList/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TodoList.Models
+{
+    public class User
+    {
+        [Key]
+        public int userId { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        public string userName { get; set; }
+
+        [Required]
+        public string firstName { get; set; }
+
+        [Required]
+        public string lastName { get; set; }
+
+        public bool active { get; set; }
+    }
+}
