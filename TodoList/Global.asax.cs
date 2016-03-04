aca5924829a3c334bd2ade2d51be7c2de71cfcc5
John Mondragon <raskaman@msn.com>
2016-03-03 23:01:07 -0500

    removing unused entries

--- TodoList/Global.asax.cs
+++ TodoList/Global.asax.cs
@@ -15,37 +15,7 @@ namespace TodoList
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-
-            InitDatabase();
-        }
-
-        void InitDatabase()
-        {
-            Database.SetInitializer<TodoListContext>(new DropCreateDatabaseIfModelChanges<TodoListContext>());
-
-            using (var context = new TodoListContext())
-            {
-                if (context.Tasks.Count() == 0)
-                {
-                    context.Tasks.Add
-                        (
-                        new Task()
-                        {
-                            title = "Need to buy my milk",
-                            lastUpdated = DateTime.Now,
-                        }
-                    );
-
-                    context.Tasks.Add
-                        (
-                        new Task()
-                        {
-                            title = "Call my wife",
-                            lastUpdated = DateTime.Now,
-                        }
-                    );
-                }
-            }
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<TodoListContext>());
         }
     }
 }
